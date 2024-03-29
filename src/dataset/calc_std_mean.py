a8294f3eb32db81a69f61eb90f01cebca0b813eb
Berke Gökmen <ahmetberkegokmen@icloud.com>
2024-03-29 20:22:50 +0300

    initial commit

---
+++ src/dataset/calc_std_mean.py
@@ -0,0 +1,36 @@
+import torch
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+import numpy as np
+
+transform = transforms.Compose(
+    [
+        transforms.Resize((224, 224)),  # Resize images to match model input
+        transforms.ToTensor(),  # Convert images to tensor
+    ]
+)
+
+dataset = datasets.ImageFolder(root="./dataset/raw-img", transform=transform)
+dataloader = DataLoader(dataset, batch_size=64, shuffle=False, num_workers=1)
+
+
+def calculate_mean_std(dataloader):
+    channels_sum, channels_squared_sum, num_batches = 0, 0, 0
+
+    for data, _ in dataloader:
+        # [B, C, W, H] => [B, C, W * H]
+        data = data.view(data.size(0), data.size(1), -1)
+
+        channels_sum += torch.mean(data, dim=[0, 2])
+        channels_squared_sum += torch.mean(data**2, dim=[0, 2])
+        num_batches += 1
+
+    mean = channels_sum / num_batches
+    std = (channels_squared_sum / num_batches - mean**2) ** 0.5
+
+    return mean, std
+
+
+mean, std = calculate_mean_std(dataloader)
+print(f"Mean: {mean}")
+print(f"Std: {std}")
