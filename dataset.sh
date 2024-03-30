549453e05cdd6161a74457a4f3c32eae587c94c2
Berke Gökmen <ahmetberkegokmen@icloud.com>
2024-03-30 16:31:53 +0300

    dataset.sh

---
+++ dataset.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 <path-to-dataset-directory>"
+  exit 1
+fi
+
+DATASET_DIR="$1"
+
+declare -A translate=(
+  ["cane"]="dog"
+  ["cavallo"]="horse"
+  ["elefante"]="elephant"
+  ["farfalla"]="butterfly"
+  ["gallina"]="chicken"
+  ["gatto"]="cat"
+  ["mucca"]="cow"
+  ["pecora"]="sheep"
+  ["scoiattolo"]="squirrel"
+  ["dog"]="cane"
+  ["horse"]="cavallo"
+  ["elephant"]="elefante"
+  ["butterfly"]="farfalla"
+  ["chicken"]="gallina"
+  ["cat"]="gatto"
+  ["cow"]="mucca"
+  ["spider"]="ragno"
+  ["squirrel"]="scoiattolo"
+)
+
+cd "$DATASET_DIR" || exit
+
+for folder in */ ; do
+  folder_name=${folder%/}
+  
+  if [[ -n "${translate[$folder_name]}" ]]; then
+    mv "$folder_name" "${translate[$folder_name]}"
+    echo "Renamed $folder_name to ${translate[$folder_name]}"
+  else
+    echo "No mapping found for $folder_name, skipping..."
+  fi
+done
+
