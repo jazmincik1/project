de127f1db60a598db86088809a4312f00a53a747
Berke Gökmen <ahmetberkegokmen@icloud.com>
2024-04-01 17:21:25 +0300

    use all images

--- src/dataset/dataset.py
+++ src/dataset/dataset.py
@@ -4,7 +4,7 @@ from PIL import Image
 
 
 class AnimalDataset(Dataset):
-    def __init__(self, root_dir, transform=None):
+    def __init__(self, root_dir, transform=None, use_all_images=False):
         self.root_dir = root_dir
         self.transform = transform
         self.images = []
@@ -12,9 +12,18 @@ class AnimalDataset(Dataset):
         self.class_names = sorted(os.listdir(root_dir))
         self.class_names = [name for name in self.class_names if os.path.isdir(os.path.join(root_dir, name))]
 
+        self.class_image_count = {}
+        for class_name in self.class_names:
+            class_dir = os.path.join(root_dir, class_name)
+            self.class_image_count[class_name] = len(os.listdir(class_dir))
+
+        min_image_count = min(self.class_image_count, key=self.class_image_count.get)
+        print(f"Minimum image count: {min_image_count} - {self.class_image_count[min_image_count]}")
+
         for idx, class_name in enumerate(self.class_names):
             class_dir = os.path.join(root_dir, class_name)
-            for img_name in os.listdir(class_dir):
+            class_dir_list = os.listdir(class_dir) if use_all_images else os.listdir(class_dir)[:min_image_count]
+            for img_name in class_dir_list:
                 if os.path.isfile(os.path.join(class_dir, img_name)):
                     self.images.append(os.path.join(class_dir, img_name))
                     self.labels.append(idx)
