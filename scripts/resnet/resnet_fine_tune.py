eb36c507138c568bd99f893a5b6424e6e4d61404
Berke Gökmen <ahmetberkegokmen@icloud.com>
2024-04-02 11:37:27 +0300

    fix path

--- scripts/resnet/resnet_fine_tune.py
+++ scripts/resnet/resnet_fine_tune.py
@@ -1,7 +1,7 @@
 import sys
 import os
 
-sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.append(os.path.join(os.path.dirname(__file__), "../.."))
 
 from torch.utils.data import DataLoader, random_split
 import torch
