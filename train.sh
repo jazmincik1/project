d2d87de1ccaf1502f922346008844ee09bbc7a3b
gencturkmert <gencturkmert1@gmail.com>
2024-04-11 15:47:15 +0300

    sh script added

--- train.sh
+++ train.sh
@@ -49,6 +49,19 @@ elif [ "$MODEL_NAME" == "lenet_train" ]; then
         --save-checkpoints 1 \
         --save-checkpoints-epoch 5
 
+elif [ "$MODEL_NAME" == "vgg_fine_tune" ]; then
+    python scripts/vgg/vgg_fine_tune.py \
+        --run-name vgg_fine_tune \
+        --dataset-dir /content/drive/MyDrive/colab_data/animals \
+        --learning-rate 0.001 \
+        --batch-size 16 \
+        --num-workers 2 \
+        --num-epochs 50 \
+        --load-checkpoints 0 \
+        --load-checkpoints-path /home \
+        --save-checkpoints 1 \
+        --save-checkpoints-epoch 10
+
 elif [ "$MODEL_NAME" == "alexnet" ]; then
     echo "not implemented yet"
 else
