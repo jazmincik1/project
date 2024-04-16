7bb55a8203cd00acebb1c4b7b640571a5a7b00c5
gencturkmert <gencturkmert1@gmail.com>
2024-04-16 21:40:14 +0300

    fixed vgg version parser

--- src/models/vgg.py
+++ src/models/vgg.py
@@ -2,19 +2,19 @@ import torchvision.models as models
 import torch.nn as nn
 
 class VGG:
-    def __init__(self, version='vgg16', num_classes=10):
+    def __init__(self, version='16', num_classes=10):
         self.version = version
         self.num_classes = num_classes
         self.model = self._construct_model()
 
     def _construct_model(self):
-        if self.version == 'vgg11':
+        if self.version == '11':
             model = models.vgg11(pretrained=True)
-        elif self.version == 'vgg13':
+        elif self.version == '13':
             model = models.vgg13(pretrained=True)
-        elif self.version == 'vgg16':
+        elif self.version == '16':
             model = models.vgg16(pretrained=True)
-        elif self.version == 'vgg19':
+        elif self.version == '19':
             model = models.vgg19(pretrained=True)
         else:
             raise ValueError(f'Unsupported VGG version: {self.version}')
