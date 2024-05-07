c4013ee9d4026da8ff163716eced00e7f83e9fd9
gencturkmert <gencturkmert1@gmail.com>
2024-05-07 22:10:34 +0300

    added different vgg settings

--- vgg.sh
+++ vgg.sh
@@ -12,3 +12,9 @@ python scripts/vgg/vgg_fine_tune.py --run-name vgg-0.001-lr-20-epochs-nodecay --
 # Finally, for 10 epochs, try 2 different batch sizes
 python scripts/vgg/vgg_fine_tune.py --run-name vgg-32-batch-10-epochs --dataset-dir /content/drive/MyDrive/colab_data/animals --learning-rate 0.001 --batch-size 32 --num-workers 2 --num-epochs 10 --save-checkpoints 1 --save-checkpoints-epoch 10
 python scripts/vgg/vgg_fine_tune.py --run-name vgg-64-batch-10-epochs --dataset-dir /content/drive/MyDrive/colab_data/animals --learning-rate 0.001 --batch-size 64 --num-workers 2 --num-epochs 10 --save-checkpoints 1 --save-checkpoints-epoch 10
+
+python scripts/vgg/vgg_fine_tune.py --run-name vgg11-0.001-lr-10-epochs --dataset-dir /content/drive/MyDrive/colab_data/animals --learning-rate 0.001 --vgg-version '11' --batch-size 16 --num-workers 2 --num-epochs 10 --save-checkpoints 1 --save-checkpoints-epoch 10
+python scripts/vgg/vgg_fine_tune.py --run-name vgg13-0.001-lr-10-epochs --dataset-dir /content/drive/MyDrive/colab_data/animals --learning-rate 0.001 --vgg-version '13' --batch-size 16 --num-workers 2 --num-epochs 10 --save-checkpoints 1 --save-checkpoints-epoch 10
+python scripts/vgg/vgg_fine_tune.py --run-name vgg19-0.001-lr-10-epochs --dataset-dir /content/drive/MyDrive/colab_data/animals --learning-rate 0.001 --vgg-version '19' --batch-size 16 --num-workers 2 --num-epochs 10 --save-checkpoints 1 --save-checkpoints-epoch 10
+
+
